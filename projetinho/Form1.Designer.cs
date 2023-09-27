badc7525ceaa41ae3976ce936c30f481beb9e204
Fellpis-AUG <feupes2000@gmail.com>
2023-09-27 12:13:17 -0300

    27/9

--- projetinho/Form1.Designer.cs
+++ projetinho/Form1.Designer.cs
@@ -28,81 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnPronto = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.txbsize = new System.Windows.Forms.Label();
-            this.txbFlavor = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
-            // btnPronto
+            // pictureBox1
             // 
-            this.btnPronto.Location = new System.Drawing.Point(159, 60);
-            this.btnPronto.Name = "btnPronto";
-            this.btnPronto.Size = new System.Drawing.Size(212, 91);
-            this.btnPronto.TabIndex = 0;
-            this.btnPronto.Text = "preparar pastel";
-            this.btnPronto.UseVisualStyleBackColor = true;
-            this.btnPronto.Click += new System.EventHandler(this.btnPronto_Click);
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-8, -25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(254, 196);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
-            // textBox1
+            // pictureBox2
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 76);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(122, 20);
-            this.textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(12, 131);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(122, 20);
-            this.textBox2.TabIndex = 2;
-            // 
-            // txbsize
-            // 
-            this.txbsize.AutoSize = true;
-            this.txbsize.Location = new System.Drawing.Point(12, 60);
-            this.txbsize.Name = "txbsize";
-            this.txbsize.Size = new System.Drawing.Size(48, 13);
-            this.txbsize.TabIndex = 5;
-            this.txbsize.Text = "tamanho";
-            // 
-            // txbFlavor
-            // 
-            this.txbFlavor.AutoSize = true;
-            this.txbFlavor.Location = new System.Drawing.Point(12, 115);
-            this.txbFlavor.Name = "txbFlavor";
-            this.txbFlavor.Size = new System.Drawing.Size(33, 13);
-            this.txbFlavor.TabIndex = 6;
-            this.txbFlavor.Text = "sabor";
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(1149, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(82, 70);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(394, 171);
-            this.Controls.Add(this.txbFlavor);
-            this.Controls.Add(this.txbsize);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.btnPronto);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1229, 552);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
-
         }
 
         #endregion
 
-        private System.Windows.Forms.Button Btnstore;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox3;
-        private System.Windows.Forms.CheckBox checkBox4;
-        private System.Windows.Forms.Button btnPronto;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label txbsize;
-        private System.Windows.Forms.Label txbFlavor;
+       
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
---
+++ projetinho/Formlogin.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace projetinho
+{
+    public partial class Formlogin : Form
+    {
+        private int Id;
+
+        public Formlogin()
+        {
+
+            InitializeComponent();
+        }
+        private void UpdateListView()
+        {
+            listView1.Items.Clear();
+
+            ClassConexao conn = new ClassConexao();
+            SqlCommand sqlCom = new SqlCommand();
+
+            sqlCom.Connection = conn.ReturnConnection();
+            sqlCom.CommandText = "SELECT Id, nome, email, numero, endereco FROM usuario";
+
+            try
+            {
+                SqlDataReader dr = sqlCom.ExecuteReader();
+
+                //Enquanto for possível continuar a leitura das linhas que foram retornadas na consulta, execute.
+                while (dr.Read())
+                {
+                    int id = (int)dr["Id"];
+                    string nome = (string)dr["nome"];
+                    string email = (string)dr["email"];
+                    string numero = (string)dr["numero"];
+                    string endereço = (string)dr["endereco"];
+                   
+
+                    ListViewItem lv = new ListViewItem(id.ToString());
+                    lv.SubItems.Add(nome);
+                    lv.SubItems.Add(email);
+                    lv.SubItems.Add(numero);
+                    lv.SubItems.Add(endereço);
+                    listView1.Items.Add(lv);
+
+                }
+                dr.Close();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+            finally
+            {
+                conn.CloseConnection();
+            }
+        }
+
+
+        private void Formlogin_Load(object sender, EventArgs e)
+        {
+            
+                UpdateListView();
+            
+            
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ClassConexao connection = new ClassConexao();
+            SqlCommand sqlCommand = new SqlCommand();
+
+            sqlCommand.Connection = connection.ReturnConnection();
+            sqlCommand.CommandText = @"INSERT INTO usuario VALUES ( @nome, @email, @numero, @endereco, @data_de_nacimento,@senha)";
+
+            sqlCommand.Parameters.AddWithValue("@nome", txt_nome.Text);
+            sqlCommand.Parameters.AddWithValue("@email", tex_email.Text);
+            sqlCommand.Parameters.AddWithValue("@numero", txt_numero.Text);
+            sqlCommand.Parameters.AddWithValue("@endereco", textEndereco.Text);
+            sqlCommand.Parameters.AddWithValue("@data_de_nacimento", DataDeNacimento.Value);
+            //sqlCommand.Parameters.AddWithValue("@data_de_nacimento", Convert.ToDateTime(DataDeNacimento));
+            sqlCommand.Parameters.AddWithValue("@senha", txtSenha.Text);
+            sqlCommand.ExecuteNonQuery();
+
+            MessageBox.Show("Cadastro com sucesso",
+                "AVISO",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+            txt_nome.Clear();
+            tex_email.Clear();
+            txt_numero.Clear();
+            textEndereco.Clear();
+            //TextDataDeNacimento.Clear();
+            txtSenha.Clear();
+
+            //ClassConexao connection = new ClassConexao();
+            //SqlCommand sqlCommand = new SqlCommand();
+
+            //sqlCommand.Connection = connection.ReturnConnection();
+            //sqlCommand.CommandText = @"UPDATE usuario SET 
+            // nome = @nome,
+            // email = @email,
+            // numero = @numero,
+            // endereco = @endereco,
+            // data_de_nacimento = @data_de_nacimento,
+            // senha = @senha
+            // WHERE id = @id";
+
+            //sqlCommand.Parameters.AddWithValue("@nome", txt_nome.Text);
+            //sqlCommand.Parameters.AddWithValue("@email", tex_email.Text);
+            //sqlCommand.Parameters.AddWithValue("@numero", txt_numero.Text);
+            //sqlCommand.Parameters.AddWithValue("@endereco", textEndereco.Text);
+            //sqlCommand.Parameters.AddWithValue("@data_de_nacimento", TextDataDeNacimento.Text);
+            //sqlCommand.Parameters.AddWithValue("@senha", txtSenha.Text);
+
+
+            //sqlCommand.ExecuteNonQuery();
+
+            //MessageBox.Show("Atuelizado com sucesso",
+            //    "AVISO",
+            //    MessageBoxButtons.OK,
+            //    MessageBoxIcon.Information);
+            //txt_nome.Clear();
+            //tex_email.Clear();
+            //txt_numero.Clear();
+            //textEndereco.Clear();
+            //TextDataDeNacimento.Clear();
+            //txtSenha.Clear();
+        }
+
+        private void confirmar_Click(object sender, EventArgs e)
+        {
+            string email = tex_email.Text;
+
+            ClassConexao connection = new ClassConexao();
+            SqlCommand sqlCommand = new SqlCommand();
+
+            sqlCommand.Connection = connection.ReturnConnection();
+            sqlCommand.CommandText = @"INSERT INTO usuario VALUES ( @nome, @email, @numero, @endereco, @data_de_nacimento, @senha)";
+
+            sqlCommand.Parameters.AddWithValue("@nome", txt_nome.Text);
+            sqlCommand.Parameters.AddWithValue("@email", tex_email.Text);
+            sqlCommand.Parameters.AddWithValue("@numero", txt_numero.Text);
+            sqlCommand.Parameters.AddWithValue("@endereco", textEndereco.Text);
+            //sqlCommand.Parameters.AddWithValue("@endereco", TextDataDeNacimento.Text);
+            sqlCommand.Parameters.AddWithValue("@data_de_nacimento", DataDeNacimento.Value);
+            sqlCommand.Parameters.AddWithValue("@senha", txtSenha.Text);
+
+            sqlCommand.ExecuteNonQuery();
+            if (IsValidEmail(email))
+            {
+                sqlCommand.ExecuteNonQuery();
+
+
+                txt_nome.Clear();
+                tex_email.Clear();
+                txt_numero.Clear();
+                textEndereco.Clear();
+                //TextDataDeNacimento.Clear();
+                txtSenha.Clear();
+
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("O endereço de e - mail é inválido");
+
+            }
+
+            }
+
+            private bool IsValidEmail(string email)
+            {
+                // Define a expressão regular para validar um endereço de e-mail
+                string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+
+                // Cria um objeto Regex com a expressão regular
+                Regex regex = new Regex(pattern);
+
+                // Usa o método Match para verificar se o email corresponde ao padrão
+                return regex.IsMatch(email);
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int index;
+            index = listView1.FocusedItem.Index;
+            Id = int.Parse(listView1.Items[index].SubItems[0].Text);
+            txt_nome.Text = listView1.Items[index].SubItems[1].Text;
+            tex_email.Text = listView1.Items[index].SubItems[2].Text;
+            txt_numero.Text = listView1.Items[index].SubItems[3].Text;
+            textEndereco.Text = listView1.Items[index].SubItems[4].Text;
+
+        }
+
+        private void TextDataDeNacimento_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void TextDataDeNacimentoo_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void deletar_Click(object sender, EventArgs e)
+        {
+            ClassConexao connection = new ClassConexao();
+            SqlCommand sqlCommand = new SqlCommand();
+
+            sqlCommand.Connection = connection.ReturnConnection();
+            sqlCommand.CommandText = @"DELETE FROM Usuarios WHERE Id = @id";
+            sqlCommand.Parameters.AddWithValue("@id", Id);
+            try
+            {
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Erro: Problemas ao excluir usuário no banco.\n" + err.Message);
+            }
+            finally
+            {
+                connection.CloseConnection();
+            }
+
+        }
+    }
+}
